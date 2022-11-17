a8fe19cb20d18898c49edc4d51a987faa6a1bc50
Tim <Tim.salokat@outlook.com>
2022-11-17 21:02:32 +0100

    Changed the Navigation menu so run build

--- frontend/app_v2/src/App.js
+++ frontend/app_v2/src/App.js
@@ -10,7 +10,8 @@ import ComponentsPage from "./pages/Components_Page";
 import Home_Page from "./pages/Home_Page";
 import Todo_Page from "./pages/Todo_Page";
 
-import {MdArticle, MdDns, MdHome} from "react-icons/md";
+import {MdArticle, MdHome} from "react-icons/md";
+import {CgMenuLeft} from "react-icons/cg";
 
 function App() {
 
@@ -43,16 +44,34 @@ function App() {
         <ComponentsPage/>
       </div>
 
-      <div id="BotBar" data-open="false"> 
+      {/* <div id="BotBar" data-open="false"> 
         <div className="IconContainer" onClick={() => Local.link("Todos")}><MdArticle id="Icon"/> </div>
         <div className="IconContainer" onClick={() => Local.link("Home")}><MdHome id="Icon"/> </div>
         <div className="IconContainer" onClick={() => Local.link("Dev")}><MdDns id="Icon"/> </div>
       </div>
       <div className="Section" id="BotButton" onClick={() => {
         Global.setMenuOpen = !Global.menuopen;
-      }}/>
+      }}/> */}
 
     <Overlay click={() => {Global.setMenuOpen = false; Global.setOverlayActive = false;}}/>
+    <div id="MenuBar">
+
+      <div className="decoration">
+        <div className="decorator" onClick={() => Local.link("Todos")}/>
+        <div className="decorator" onClick={() => Local.link("Home")}/>
+      </div>
+
+      <MdArticle id="Icon"/>
+      <label>Todos</label>
+      <MdHome id="Icon"/>
+      <label>Home</label>
+
+      <CgMenuLeft id="MenuIcon"/>
+
+      {/* <div className="IconContainer" onClick={() => Local.link("Todos")}> </div>
+      <div className="IconContainer" onClick={() => Local.link("Home")}><MdHome id="Icon"/> </div> */}
+
+    </div>
     </div>
 
     </>
