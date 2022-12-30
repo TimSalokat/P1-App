52c5b5014d89162befb1584f76be2cfe01a078cd
Tim <tim.salokat@outlook.com>
2022-12-30 14:40:00 +0100

    Deleted old folders

--- frontend/p1-app-old/android/app/src/main/java/io/ionic/devBuild_a1/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package io.ionic.devBuild_a1;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- frontend/p1-app/android/app/src/main/java/io/ionic/p1_dev_build/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package io.ionic.p1_dev_build;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
